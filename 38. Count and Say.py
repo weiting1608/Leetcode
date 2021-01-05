b2a731b4dc80d9fdc63c37a3a18c579f50783753
weiting1608 <weiting1608@gmail.com>
2021-01-04 20:05:55 -0500

    add 38 count and say

--- 38. Count and Say.py
+++ 38. Count and Say.py
@@ -1,5 +1,6 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
+        # initializa an array with n zero.
         memo = [0 for _ in range(n+1)]
         memo[1] = "1"
         for i in range(2, n+1):
@@ -7,15 +8,15 @@ class Solution:
         return memo[n]
     
     def say(self, s):
-        i = j = 0
+        i, j = 0, 1
         res = ""
         while j < len(s):
+            # when not equal, means the equal number has been traversed, need to summary count and value.
             if s[i] != s[j]:
                 res += str(j-i) + s[i]
                 i = j
             j += 1
+        # last group, no need to compare, just attach j-i times of s[i]
         res += str(j-i) + s[i]
         return res
         
-sol = Solution()
-print(sol.countAndSay(11))
