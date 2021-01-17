b2c91ab38400febe179ff7d8e196e647e9cf32d9
weiting1608 <weiting1608@gmail.com>
2021-01-17 15:01:57 -0500

    update trim BST

--- 669. Trim a Binary Search Tree.py
+++ 669. Trim a Binary Search Tree.py
@@ -1,23 +1,25 @@
 # Definition for a binary tree node.
-import collections
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-class TreeNode:
-    def __init__(self, val = 0, left = None, right = None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-tree2 = TreeNode(2)
-tree3 = TreeNode(3)
-tree1 = TreeNode(1, tree2, tree3)
-
-nodes = collections.deque([tree1])
-if tree1.left:
-    print(tree1.left.val)
-if tree1.right:
-    print(tree1.right.val)
-
-print(tree1.val)
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        if not root: return
+        # case 1: root and root左子树都不能要了
+        if root.val < low: return self.trimBST(root.right, low, high)
+        # case 2: root and root右子树都不能要了
+        elif root.val > high: return self.trimBST(root.left, low, high)
+        # case 3: root值合适，更新左子树和右子树
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
+        
+        
+        
 
 
 
